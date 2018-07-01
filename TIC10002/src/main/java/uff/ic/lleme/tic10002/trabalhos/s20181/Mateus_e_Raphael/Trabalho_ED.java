d74542b348504227617d399e441370c41edd08fa
Luiz Leme <lapaesleme@Mac-mini-de-Luiz-Leme.local>
2018-07-01 10:24:51 -0300

    ok

---
+++ TIC10002/src/main/java/uff/ic/lleme/tic10002/trabalhos/s20181/Mateus_e_Raphael/Trabalho_ED.java
@@ -0,0 +1,16 @@
+package uff.ic.lleme.tic10002.trabalhos.s20181.Mateus_e_Raphael;
+
+/**
+ *
+ * @author raphael
+ */
+public class Trabalho_ED {
+
+    public static void main(String[] args) {
+
+        FilaPrioridade fila = new FilaPrioridade();
+        fila.atendimento();
+
+    }
+
+}
