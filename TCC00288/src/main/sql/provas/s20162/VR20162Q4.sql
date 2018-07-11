224569432128fa3176a6d4c3020f503833bdadd1
lapaesleme <lapaesleme@ic.uff.br>
2018-07-11 10:45:35 -0300

    OK

--- TCC00288/src/main/sql/provas/s20162/VR20162Q4.sql
+++ TCC00288/src/main/sql/provas/s20162/VR20162Q4.sql
@@ -1,27 +1,22 @@
+DROP TABLE IF EXISTS hotel cascade;
 CREATE TABLE hotel (
-  numero integer NOT NULL,
-  nome TEXT NOT NULL,
-  CONSTRAINT hotel_pk PRIMARY KEY
-  (numero));
+  numero integer NOT NULL primary key,
+  nome TEXT NOT NULL);
 
+DROP TABLE IF EXISTS reserva cascade;
 CREATE TABLE reserva (
-  numero integer NOT NULL,
-  hotel integer NOT NULL,
+  numero integer NOT NULL primary key,
+  hotel integer NOT NULL REFERENCES hotel (numero),
   cpf_cnpj integer NOT NULL,
   inicio timestamp not null,
-  fim timestamp not null,
-  CONSTRAINT reserva_pk PRIMARY KEY
-  (numero),
-  CONSTRAINT reserva_hotel_fk FOREIGN KEY
-  (hotel) REFERENCES hotel (numero));
+  fim timestamp not null);
 
+DROP TABLE IF EXISTS estadia cascade;
 CREATE TABLE estadia (
-  numero integer NOT NULL,
+  numero integer NOT NULL primary key,
   quarto text not null,
   inicio timestamp not null,
   fim timestamp,
-  CONSTRAINT estadia_pk PRIMARY KEY
-  (numero),
   CONSTRAINT estadia_reserva_fk FOREIGN KEY
   (numero) REFERENCES reserva (numero)
   on delete restrict on update cascade);
@@ -34,10 +29,10 @@ DECLARE
 BEGIN
     SELECT inicio, fim INTO v_inicio,v_fim FROM reserva WHERE numero = NEW.numero;
 
-    IF (NOT (NEW.inicio BETWEEN v_inicio AND (v_inicio + $$1 day$$::INTERVAL)
-        AND (NEW.fim BETWEEN v_inicio AND v_fim OR NEW.fim IS NULL)
-        AND (NEW.inicio < NEW.fim OR NEW.fim IS NULL)) THEN
-        RAISE EXCEPTION $$Erro: periodo de estadia invalido!$$;
+    IF (NOT (NEW.inicio BETWEEN v_inicio AND (v_inicio + '1 day'::INTERVAL)
+            AND NEW.fim BETWEEN v_inicio AND v_fim OR NEW.fim IS NULL
+            AND NEW.inicio < NEW.fim OR NEW.fim IS NULL)) THEN
+        RAISE EXCEPTION 'Erro: periodo de estadia invalido!';
     END IF;
 
     RETURN NEW;
@@ -54,11 +49,11 @@ DECLARE
 BEGIN
     SELECT inicio, fim INTO v_inicio,v_fim FROM estadia WHERE numero = OLD.numero;
 
-    IF (NOT (FOUND
-        AND (v_inicio BETWEEN NEW.inicio AND (NEW.inicio + $$1 day$$::INTERVAL))
-        AND (v_fim BETWEEN NEW.inicio AND NEW.fim OR v_fim IS NULL)
-        AND (NEW.inicio < NEW.fim))) THEN
-        RAISE EXCEPTION $$Erro: periodo de estadia incompativel com reserva!$$;
+    IF (FOUND AND
+        (NOT (v_inicio BETWEEN NEW.inicio AND (NEW.inicio + '1 day'::INTERVAL)
+             AND v_fim BETWEEN NEW.inicio AND NEW.fim OR v_fim IS NULL
+             AND NEW.inicio < NEW.fim))) THEN
+        RAISE EXCEPTION 'Erro: periodo de estadia incompativel com reserva!';
     END IF;
 
     RETURN NEW;
