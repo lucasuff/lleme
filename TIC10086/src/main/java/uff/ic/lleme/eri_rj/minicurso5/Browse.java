0e862c08f5410b1309690c078f0b430c5e444968
Luiz Leme <lapaesleme@ic.uff.br>
2019-03-09 19:13:11 -0300

    Update Browse.java

--- TIC10086/src/main/java/uff/ic/lleme/eri_rj/minicurso5/Browse.java
+++ TIC10086/src/main/java/uff/ic/lleme/eri_rj/minicurso5/Browse.java
@@ -1,37 +1,39 @@
 package uff.ic.lleme.eri_rj.minicurso5;
 
-import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.util.FileManager;
-import org.apache.jena.util.iterator.ExtendedIterator;
+import org.apache.jena.vocabulary.RDF;
 
 public class Browse {
 
     public static void main(String[] args) {
         String rdfTranslatorService = "http://rdf-translator.appspot.com/convert/rdfa/xml/%1$s";
-        String url = "http://swlab.lleme.net:8080/cursos.html";
+        String dataURL = "http://swlab.lleme.net:8080/cursos.html";
         String schemaURL = "http://swlab.lleme.net:8080/vocab/teaching#Course";
 
         // Browsing
         Model data = ModelFactory.createDefaultModel();
-        RDFDataMgr.read(data, String.format(rdfTranslatorService, url), Lang.RDFXML);
+        RDFDataMgr.read(data, String.format(rdfTranslatorService, dataURL), Lang.RDFXML);
         //RDFDataMgr.write(System.out, data, Lang.N3);
 
         //Detecting Events
-        Model schema = FileManager.get().loadModel("http://swlab.lleme.net:8080/vocab/teaching#Course");
+        OntModel schema = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        schema.read(schemaURL);
+
         Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
         reasoner = reasoner.bindSchema(schema);
-        InfModel infmodel = ModelFactory.createInfModel(reasoner, data);
-        OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, infmodel);
-        ExtendedIterator<Individual> iter = ontModel.listIndividuals(schema.getResource("http://www.w3.org/2002/12/cal/icaltzd.rdf#Vevent"));
+        InfModel infModel = ModelFactory.createInfModel(reasoner, data);
+        //RDFDataMgr.write(System.out, infModel, Lang.N3);
+
+        ResIterator iter = infModel.listSubjectsWithProperty(RDF.type, "http://swlab.lleme.net:8080/vocab/teaching#Course");
         while (iter.hasNext())
             System.out.println(iter.next().getURI());
 
