99d926e2c12a7fe07c0fbb88bb1d6b563bae1065
lapaesleme <lapaesleme@ic.uff.br>
2019-03-11 17:43:07 -0300

    Update CalendarAgent.java

--- TIC10086/src/main/java/uff/ic/lleme/tic10086/examples/CalendarAgent.java
+++ TIC10086/src/main/java/uff/ic/lleme/tic10086/examples/CalendarAgent.java
@@ -2,6 +2,8 @@ package uff.ic.lleme.tic10086.examples;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.jena.query.QueryExecution;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
@@ -11,6 +13,9 @@ import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.WebContent;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 import org.apache.jena.sparql.vocabulary.FOAF;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.VOID;
@@ -19,7 +24,7 @@ import uff.ic.swlab.vocab.teaching.Teaching;
 public class CalendarAgent {
 
     public static void main(String[] args) {
-        String meuNome = "Mateus";
+        String nomeAluno = "LUAN SIMOES CARDOSO";
         String rdfTranslatorService = "http://rdf-translator.appspot.com/convert/rdfa/xml/%1$s";
         String sourceURL = "http://swlab.lleme.net:8080/cursos.html";
         String schemaURL = "http://swlab.lleme.net:8080/vocab/teaching#Course";
@@ -75,16 +80,14 @@ public class CalendarAgent {
         // **************************************************************************************************************
 
         // **************************************************************************************************************
-        // Fourth step: query de SPARQL endpoints do find iCal events
+        // Fourth step: query de SPARQL endpoints to get Assignments
         //
+        for (String sparqlEndpoint : sparqlEndpoints) {
+            Model assigments = getAssigments(sparqlEndpoint, nomeAluno);
+            RDFDataMgr.write(System.out, assigments, RDFFormat.TURTLE_PRETTY);
+        }
         // **************************************************************************************************************
-        //Detecting Events
-        //OntModel schema = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-        //schema.read(schemaURL);
-        //Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-        //reasoner = reasoner.bindSchema(schema);
-        //InfModel infModel = ModelFactory.createInfModel(reasoner, data);
-        //RDFDataMgr.write(System.out, infModel, Lang.N3);
+
     }
 
     /**
@@ -134,7 +137,59 @@ public class CalendarAgent {
         return sparqlEndpointURL;
     }
 
-    private static Model getICalEvent(String sparqlEndpoint) {
-        return null;
+    private static Model getAssigments(String sparqlEndpoint, String nome) {
+        String query = ""
+                + "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                + "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+                + "PREFIX text: <http://jena.apache.org/text#>\n"
+                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+                + "prefix void: <http://rdfs.org/ns/void#>\n"
+                + "prefix icaltzd: <http://www.w3.org/2002/12/cal/icaltzd#>\n"
+                + "prefix particip: <http://purl.org/vocab/participation/schema#>\n"
+                + "prefix myvoid: <http://swlab.lleme.net:8080/void.ttl#> \n"
+                + "prefix teach: <http://swlab.lleme.net:8080/vocab/teaching#>\n"
+                + "prefix rsc: <http://swlab.lleme.net:8080/resource/>\n"
+                + "prefix BR: <http://www.w3.org/2002/12/cal/tzd/America/Sao_Paulo#>\n"
+                + "\n"
+                + "construct {\n"
+                + "  ?asmt ?p ?o.\n"
+                + "  ?o ?p2 ?o2.\n"
+                + "  ?group particip:role ?role.\n"
+                + "  ?role particip:role_at ?group.\n"
+                + "  ?group teach:hasAssignment ?asmt.\n"
+                + "  ?role rdfs:label ?lbl.\n"
+                + "}\n"
+                + "where {\n"
+                + "  {\n"
+                + "    select ?sdt ?score\n"
+                + "    where {\n"
+                + "      {?sdt a foaf:Person. (?sdt ?score) text:query \"(%1$s)\".}\n"
+                + "    	union {graph ?g {?sdt a foaf:Person. (?sdt ?score) text:query \"(%1$s)\".}}\n"
+                + "    }\n"
+                + "    order by desc(?score)\n"
+                + "    limit 1\n"
+                + "  }\n"
+                + "  {{?role particip:holder ?sdt} \n"
+                + "    union {graph ?g1 {?role particip:holder ?sdt}}}\n"
+                + "  {{?role a teach:UndergraduateStudent} \n"
+                + "    union {graph ?g2 {?role a teach:UndergraduateStudent}}\n"
+                + "    union {?role a teach:Teacher} \n"
+                + "    union {graph ?g2 {?role a teach:Teacher}}}\n"
+                + "  {{?group particip:role ?role}\n"
+                + "    union {graph ?g3 {?group particip:role ?role}}}\n"
+                + "  {{?group teach:hasAssignment ?asmt}\n"
+                + "    union {graph ?g4 {?group teach:hasAssignment ?asmt}}}\n"
+                + "  {{?asmt ?p ?o.} union {graph ?g5 {?asmt ?p ?o.}}}\n"
+                + "  optional {{?o ?p2 ?o2} union {graph ?g6 {?o ?p2 ?o2}}}\n"
+                + "  optional {{?role rdfs:label ?lbl} union {graph ?g7 {?role rdfs:label ?lbl}}}\n"
+                + "}";
+        query = String.format(query, nome);
+
+        final Model result = ModelFactory.createDefaultModel();
+        try (final QueryExecution exec = new QueryEngineHTTP(sparqlEndpoint, query, HttpClients.createDefault())) {
+            ((QueryEngineHTTP) exec).setModelContentType(WebContent.contentTypeRDFXML);
+            exec.execConstruct(result);
+        }
+        return result;
     }
 }
