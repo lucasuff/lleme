ed153f86ca05ea514412fbe09d05ab7a8d087838
lapaesleme <lapaesleme@ic.uff.br>
2019-09-25 10:36:21 -0300

    Update rascunho.sql

--- TCC00288/src/main/sql/aulas/rascunho.sql
+++ TCC00288/src/main/sql/aulas/rascunho.sql
@@ -1,38 +1,36 @@
-create table produto(
-  id bigint not null,
-  nome varchar not null);
+CREATE OR REPLACE FUNCTION estado(acoes varchar[])
+RETURNS table(acao varchar, qtd integer, preco float)
+AS $$
 
-create table venda(
-  _data timestamp not null,
-  produto bigint not null,
-  qtd integer not null);
- 
--- usar d1 e d2
-CREATE OR REPLACE FUNCTION best_sellers(d1 date, d2 date) RETURNS
-TABLE(anomes int, lista varchar[]) AS $$
-    DECLARE
-        curs1 CURSOR FOR WITH S1 AS SELECT extract(year from _data)*100 +
-        extract(month from _data) as ano_mes, produto, sum(qtd) as _qtd FROM venda
-        GROUP BY extract(year from _data)*100 + extract(month from _data), produto;
+DECLARE
+    curs CURSOR(sigla varchar) for SELECT * FROM operacao WHERE acao = sigla;
+    auxiliar operacao%rowtype[] = '{}';
+    valor_auxiliar integer = 0;
 
-        SELECT ano_mes, avg(_qtd) as media from S1
-        GROUP BY ano_mes;
-
-        curs2(anoMes int, media float) CURSOR FOR
-        SELECT produto.nome as nomeProd
-        FROM venda INNER JOIN produto ON id = venda.produto
-        WHERE ano_mes = anoMes
-        GROUP BY produto HAVING sum(qtd) > 1.6 * media;
-
-        bestProd varchar[] = '{}';
-
-    BEGIN
-        FOR linha1 in curs1 LOOP
-            bestProd = '{}';
-            FOR linha2 in curs2(linha1.ano_mes, linha1.media) LOOP
-                bestProd = array_append(bestProd, linha2.nomeProd);
-            END LOOP;
-        RETURN QUERY SELECT linha1.ano_mes, bestProd;
+BEGIN
+    FOR EACH r IN acoes LOOP
+        FOR linha IN curs(r) LOOP
+            IF linha.qtd > 0 THEN
+                auxiliar = array_append(auxiliar,linha);
+            ELSE
+                valor_auxiliar = linha.qtd;
+                FOR EACH val IN auxiliar LOOP
+                    IF valor_auxiliar >= val.qtd THEN
+                        valor_auxiliar = linha.qtd - val.qtd;
+                        val.qtd = 0;
+                    ELSE
+                        val.qtd = linha.qtd - valor_auxiliar;
+                    END IF;
+                END LOOP;
+            END IF;
         END LOOP;
-    END;
-$$ LANGUAGE plpgsql;
+        FOR EACH val IN auxiliar LOOP
+            IF val.qtd > 0 THEN
+                RETURN QUERY SELECT val.acao,val.qtd,val.preco;
+            END IF;
+        END LOOP;
+        val = '{}';
+    END LOOP;
+END;
+
+$$ LANGUAGE PLPGSQL;
