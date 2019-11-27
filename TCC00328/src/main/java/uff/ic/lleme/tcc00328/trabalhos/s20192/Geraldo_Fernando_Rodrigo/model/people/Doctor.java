e495606c97fccb75e517b0cefbfa49c3627a7370
Geraldo Fada <geraldofada.neto@gmail.com>
2019-11-27 20:09:00 -0300

    Trabalho Geraldo Fernando Rodrigo

---
+++ TCC00328/src/main/java/uff/ic/lleme/tcc00328/trabalhos/s20192/Geraldo_Fernando_Rodrigo/model/people/Doctor.java
@@ -0,0 +1,28 @@
+package uff.ic.lleme.tcc00328.trabalhos.s20192.Geraldo_Rodrigo_Fernando.model.people;
+
+import model.misc.Cpf;
+
+import java.util.ArrayList;
+
+public class Doctor extends Staff{
+    private ArrayList<String> specialties;
+
+    public Doctor(String name,
+                  Cpf cpf,
+                  String birthDate,
+                  String bloodType,
+                  String gender,
+                  ArrayList<String> specialties
+    ) {
+        super(name, cpf, birthDate, bloodType, gender);
+        this.specialties = specialties;
+    }
+
+    public ArrayList<String> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(ArrayList<String> specialties) {
+        this.specialties = specialties;
+    }
+}
