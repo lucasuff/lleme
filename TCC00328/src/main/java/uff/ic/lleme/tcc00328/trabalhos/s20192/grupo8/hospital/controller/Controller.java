ecdcbf2a817568b22fe916726538caea98b3051e
lapaesleme <lapaesleme@ic.uff.br>
2019-12-01 18:25:23 -0300

    ok

--- TCC00328/src/main/java/uff/ic/lleme/tcc00328/trabalhos/s20192/grupo8/hospital/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package uff.ic.lleme.tcc00328.trabalhos.s20192.Geraldo_Fernando_Rodrigo.hospital.controller;
-
-import uff.ic.lleme.tcc00328.trabalhos.s20192.Geraldo_Fernando_Rodrigo.hospital.db.Db;
-
-class Controller {
-
-    static Db dataBase = new Db();
-}
